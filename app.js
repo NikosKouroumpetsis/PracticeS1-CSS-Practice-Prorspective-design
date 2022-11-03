9062bb3fbee455e6a552ebad1436b708976fca1e
Nikolaos Kouroumpetsis <nikolaos.kouroumpetsis@gmail.com>
2022-11-03 21:29:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      perspective: 100,
+      rotateX: 0,
+      rotateY: 0,
+      rotateZ: 0,
+    };
+  },
+  computed: {
+    box() {
+      return (
+        "transform: perspective(" +
+        this.perspective +
+        "px) rotateX(" +
+        this.rotateX +
+        "deg) rotateY(" +
+        this.rotateY +
+        "deg) rotateZ(" +
+        this.rotateZ +
+        "deg)"
+      );
+    },
+  },
+
+  methods: {
+    reset() {
+      this.perspective = 100;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    async copy() {
+      let text = `transform:${this.box};`;
+      await navigator.clipboard.writeText(text);
+
+      alert("CSS copied successfully");
+    },
+  },
+}).mount("#app");
